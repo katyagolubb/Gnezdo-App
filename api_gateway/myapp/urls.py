29bd4b633caeaa566a1282c575b6b8c0c51afea7
haliki <khalikova.milausha@gmail.com>
2025-05-23 06:25:18 +0300

    new api-gateway aka krakend

--- api_gateway/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user/<path:path>', views.UserManagementProxyView.as_view(), name='user_management_proxy'),
-    path('book/<path:path>', views.BookApiProxyView.as_view(), name='book_api_proxy'),
-    path('health/', views.health_check, name='health_check'),
-]
