16f36981c9df05b3c2d936f76a761f2d6d1541ad
katyagolubb <katyago1ub@mail.ru>
2025-05-23 04:07:18 +0300

    Remove old book-api directory from Git index

--- book-api/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
