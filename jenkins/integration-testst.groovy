8d2738341989c2629b98dde493065b962ea08dc6
Alexey Gorohov <salkwalk@mail.ru>
2026-03-05 21:15:57 +0300

    Fix pipeline

--- jenkins/integration-testst.groovy
+++ jenkins/integration-testst.groovy
@@ -2,27 +2,23 @@ pipeline {
     agent any
 
     stages {
+
         stage('Start Docker services') {
             steps {
                 sh '''
-                    set -e
                     docker compose down -v || true
                     docker compose up -d --build
+                    sleep 20
                 '''
             }
         }
 
-        stage('Setup Python virtualenv') {
+        stage('Setup Python') {
             steps {
                 sh '''
-                    set -e
                     cd api_gateway
-
-                    # Создаём или переиспользуем окружение
                     python3 -m venv .venv
                     . .venv/bin/activate
-
-                    pip install --upgrade pip
                     pip install -r requirements.txt
                 '''
             }
@@ -31,12 +27,14 @@ pipeline {
         stage('Run integration tests') {
             steps {
                 sh '''
-                    set -e
                     cd api_gateway
                     . .venv/bin/activate
 
                     export GATEWAY_URL=http://host.docker.internal:8000
-                    pytest -q test_integration_api_gateway.py -m integration
+
+                    pytest -m integration \
+                        --junitxml=report.xml \
+                        --alluredir=allure-results
                 '''
             }
         }
@@ -44,10 +42,15 @@ pipeline {
 
     post {
         always {
-            sh '''
-                docker compose down -v || true
-            '''
+
+            junit 'api_gateway/report.xml'
+
+            allure([
+                includeProperties: false,
+                results: [[path: 'api_gateway/allure-results']]
+            ])
+
+            sh 'docker compose down -v || true'
         }
     }
 }
-
